42c488463783f729623ff4d067f6b845f8ee941a
Faure Adrien <adrien.faure2@gmail.com>
2020-07-03 14:19:24 +0200

    repo: update nix-shell and readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Running the application on Nixos
+
+Enter into the shell.
+
+```
+NIXPKGS_ALLOW_UNFREE=1 ns shell_test.nix
+```
+
+## Run on your phone
+
+Launch the android server: `adb start-server`.
+Then to run the application on your phone, use in a terminal: `flutter run`
+
+## Start emulator with android-studio
+
+launch android-studio and configure+lauch an emulator.
+
+## Launching on emulator  (**WIP**)
+
+To build the emulator use: `nix-build emulate.nix`.
+Then start the emulator: `./result/bin/run-test-emulator`.
+
+# Notes
+
+For some reasons, accepting adroid license causes `allowUnfree = true`, one quick workaround is to run the shell with: `NIXPKGS_ALLOW_UNFREE=1 nix-shell shell_test.nix`.
--- shell.nix
+++ shell.nix
@@ -1,40 +1,77 @@
-# Shell pined on stable channel
-{ pkgs ? import (fetchTarball https://channels.nixos.org/nixos-20.03/nixexprs.tar.xz) { } }:
+# https://specific.solutions.limited/projects/hanging-plotter/android-environment.md
+{ pkgs ? import (fetchTarball https://channels.nixos.org/nixos-20.03/nixexprs.tar.xz) {
+    config.android_sdk.accept_license = true;
+  }
+}:
 let
-  # Date: 25 Jun 2020
-  # Current commit: https://github.com/NixOS/nixpkgs-channels/archive/22a81aa5fc15b2d41b12f7160a71cd4a9f3c3fa1.tar.xz
-  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+  allowUnfree = true;
+  # flutterPkgs = (import (builtins.fetchTarball "https://github.com/babariviere/nixpkgs/archive/flutter-testing.tar.gz")  {});
+  unstablePkgs = (import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz")   {});
+  extensions = (with unstablePkgs.vscode-extensions; [
+      bbenoist.Nix
+  ]) ++ unstablePkgs.vscode-utils.extensionsFromVscodeMarketplace [({
+      name = "flutter";
+      publisher = "dart-code";
+      version = "3.11.0";
+      sha256 = "sha256:02c47ly7yb0bpsgr2ba17wx581baz9n1x16zb3whk321nbjawz7i";
+  })
+  ({
+      name = "dart-code";
+      publisher = "dart-code";
+      version = "3.11.0";
+      sha256 = "sha256:0rgszz5iw6ja2cjmg3vl1m1a4392rkykcw2n7xskf1pgc73kf1h0";
+  })];
 
-  # extensions = (with pkgs.vscode-extensions; [
-  #     bbenoist.Nix
-  # ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [({
-  #     name = "flutter";
-  #     publisher = "dart-code";
-  #     version = "3.11.0";
-  #     sha256 = "sha256:02c47ly7yb0bpsgr2ba17wx581baz9n1x16zb3whk321nbjawz7i";
-  # })
-  # ({
-  #     name = "dart-code";
-  #     publisher = "dart-code";
-  #     version = "3.11.0";
-  #     sha256 = "sha256:0rgszz5iw6ja2cjmg3vl1m1a4392rkykcw2n7xskf1pgc73kf1h0";
-  # })];
+  vscode-with-extensions = unstablePkgs.vscode-with-extensions.override {
+      vscodeExtensions = extensions;
+  };
 
-  # vscode-with-extensions = pkgs.vscode-with-extensions.override {
-  #     vscodeExtensions = extensions;
-  # };
+  # This is an ugly workaround because the author of flutter's package rename
+  # flutter binary to flutter-variantname, which make flutter undetectable for
+  # editors which are looking for `flutter`.
+  mkFlutter = opts: pkgs.callPackage
+    (import "${unstablePkgs.path}/pkgs/development/compilers/flutter/flutter.nix" opts) { };
+
+  getPatches = dir:
+    let files = builtins.attrNames (builtins.readDir dir);
+    in map (f: dir + ("/" + f)) files;
+
+  flutter-dev = mkFlutter rec {
+    pname = "flutter";
+    channel = "stable";
+    version = "1.17.5";
+    filename = "flutter_linux_${version}-${channel}.tar.xz";
+    sha256Hash = "sha256:0kapja3nh7dfhjbn2np02wghijrjnpzsv4hz10fj54hs8hdx19di";
+    patches = getPatches "${unstablePkgs.path}/pkgs/development/compilers/flutter/patches/beta";
+  };
+
+  androidSdk = pkgs.androidenv.androidPkgs_9_0.androidsdk;
+
 in
-with pkgs; mkShell {
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    unstablePkgs.dart
+    flutter-dev
+    vscode-with-extensions
+    # unstablePkgs.flutterPackages.dev
+    unstablePkgs.android-studio
 
-  buildInputs = [
-    # vscode-with-extensions
-    # unstable.flutter
+    # androidSdk
+
     androidenv.androidPkgs_9_0.platform-tools
-    androidStudioPackages.beta
+    jdk
+    git
     adb-sync
     sqlite
     # Nedeed to run flutter doctor on vscode
     unzip
   ];
 
+  shellHook=''
+    export USE_CCACHE=1
+    export ANDROID_JAVA_HOME=${pkgs.jdk.home}
+    # export ANDROID_SDK_ROOT=${androidSdk}/libexec/android-sdk
+    # export ANDROID_HOME=$ANDROID_SDK_ROOT
+    export FLUTTER_SDK=${unstablePkgs.flutter.unwrapped}
+  '';
 }
