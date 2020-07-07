d84eb011c8160ef834998b08e698ccde72841d71
Faure Adrien <adrien.faure2@gmail.com>
2020-07-07 11:57:16 +0200

    code: date format

--- lib/journal/journalView.dart
+++ lib/journal/journalView.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:open_weight/common/helpers.dart';
 import 'package:provider/provider.dart';
 // Internal dependencies
 import 'package:open_weight/common/ui.dart';
 import 'package:open_weight/application_drawer.dart';
+import 'package:open_weight/journal/dayCard.dart';
 import 'package:open_weight/journal/calorieMeter.dart';
 import 'package:open_weight/journal/mealCard.dart';
-import 'package:open_weight/journal/todayCard.dart';
 import '../database/db_helper.dart';
+
 
 class JournalView extends StatelessWidget {
   final title = "Journal";
@@ -22,7 +24,7 @@ class JournalView extends StatelessWidget {
         drawer: ApplicationDrawer(),
         body: Consumer<MyDatabase>(builder: (builder, database, child) {
           return ListView(children: [
-            DayCard(),
+            DayCard(date: today()),
             Divider(height: 2, color: Colors.black,),
             CalorieMeter(),
             MealCard(
