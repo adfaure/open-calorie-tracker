d95eeb4b909a7983ad834663c75830778136c710
Faure Adrien <adrien.faure2@gmail.com>
2020-08-01 01:43:04 +0200

    repo: add copyright on files

--- lib/common/columnBuilder.dart
+++ lib/common/columnBuilder.dart
@@ -1,3 +1,20 @@
+/*  This code is part of the open calorie tracker project. 
+ *   This application helps you to track the consumed calories on a daily basis.
+ *   Copyright (C) 2020. Adrien FAURE
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 // https://gist.github.com/slightfoot/a75d6c368f1b823b594d9f04bf667231
 import 'package:flutter/material.dart';
 
@@ -31,10 +48,10 @@ class CustomColumnBuilder extends StatelessWidget {
         verticalDirection: this.verticalDirection,
         children: ListTile.divideTiles(
           context: context,
-          color: Colors.black54,          
+          color: Colors.black54,
           tiles: List.generate(
-                  this.itemCount, (index) => this.itemBuilder(context, index)).toList()
-              ,
+                  this.itemCount, (index) => this.itemBuilder(context, index))
+              .toList(),
         ).toList());
   }
 }
