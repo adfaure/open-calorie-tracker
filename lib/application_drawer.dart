1a857813963dd2e51527fb102d39197c917c4a6d
Faure Adrien <adrien.faure2@gmail.com>
2020-08-02 23:16:20 +0200

    code: remove barcode code example

--- lib/application_drawer.dart
+++ lib/application_drawer.dart
@@ -20,7 +20,6 @@ import 'package:flutter/material.dart';
 import 'package:moor_db_viewer/moor_db_viewer.dart';
 import 'package:provider/provider.dart';
 
-import 'common/barcode.dart';
 import 'database/db_helper.dart';
 import 'food/food.dart';
 import 'food/openfoodfacts.dart';
@@ -62,12 +61,6 @@ class ApplicationDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            title: Text('Barcode debug'),
-            onTap: () {
-              _navigateBarcodeDebug(context);
-            },
-          ),
-          ListTile(
             title: Text('Debug data'),
             onTap: () {
               _debugShowDatabase(context);
@@ -106,13 +99,6 @@ class ApplicationDrawer extends StatelessWidget {
     await Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => OpenFoodFacts()),
-    );
-  }
-
-  _navigateBarcodeDebug(BuildContext context) async {
-    await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => BarcodeTest()),
     );
   }
 }
