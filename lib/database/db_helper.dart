77692eb72ceea2c78e304af9f2b3c3acfff22c90
Faure Adrien <adrien.faure@protonmail.com>
2020-12-28 20:35:14 +0100

    repo: fix dependency issues

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -18,6 +18,7 @@
 
 import 'package:flutter/rendering.dart';
 import 'package:moor_ffi/moor_ffi.dart';
+// import 'package:moor/ffi.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'package:moor/moor.dart';
